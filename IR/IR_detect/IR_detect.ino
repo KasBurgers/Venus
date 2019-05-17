7e06261ec6bbb4c9862937d16bdfc180fa4fb546
Egor-Kovalenko <50663648+Egor-Kovalenko@users.noreply.github.com>
2019-05-17 11:10:43 +0200

    Update IR_detect.ino

--- IR/IR_detect/IR_detect.ino
+++ IR/IR_detect/IR_detect.ino
@@ -2,34 +2,34 @@
 // define max voltage
 
 
-int ir_detect_volt(char[2] pinName)
+int ir_detect_volt(char pinName)
 {
       int voltage_max=5000;
       int voltage_step=voltage_max/1024;
-      int in=0;
       int in=analogRead(pinName);
       
-      voltage_ir=ir*voltage_step;
+      int voltage_ir=in*voltage_step;
       return voltage_ir;
 }
 
-int ir_detect_rock(char[2] pinName){
+int ir_detect_rock(char pinName){
 
-      inputVolt=ir_detect_volt(pinName)
+      int detect;
+      int inputVolt=ir_detect_volt(pinName);
       if (inputVolt>1e3){
-        detect=1
+        detect=1;
       }
       else {detect=0;}
       return detect;
 }
 
-int ir_detect_border(char[2] pinName){
-
-      inputVolt=ir_detect_volt(pinName)
-      if inputVolt<600{
-        detect=1
+int ir_detect_border(char pinName){
+      int detect;
+      int inputVolt=ir_detect_volt(pinName);
+      if (inputVolt<600){
+        detect=1;
       }
-      else {detect=0}
+      else {detect=0;}
       return detect;
 }
 
