452150da1c5ebfa8320dcc4de7f594a57cace7f7
KasBurgers <kasburgers@gmail.com>
2019-06-05 16:15:56 +0200

    ir trunk + delay in picking up sketch

--- Libraries/Sensor/Definitions_sensor.h
+++ Libraries/Sensor/Definitions_sensor.h
@@ -8,7 +8,7 @@
 #define pin_servo_head 11
 #define pin_wheel_left 12
 #define pin_wheel_right 13
-#define pin_IR_rock 100		
+#define pin_ir_trunk 0		
 #define pin_ir_ground_1 1
 #define pin_ir_ground_2 2
 #define pin_servo_grabber 10
--- Libraries/Sensor/Sensors.h
+++ Libraries/Sensor/Sensors.h
@@ -7,11 +7,14 @@ class Sensors{
 	static int ultrasound();
 	static bool ir_ground_left();
 	static bool ir_ground_right();
+	static bool ir_trunk();
     static int ultrasoundReturn;
     static bool ir_ground_return_left;
     static bool ir_ground_return_right;
+    static bool ir_trunkReturn;
+    
 	private:
-		static int ir_voltage(int pin);
+	static int ir_voltage(int pin);
     static int voltage_ir;
 };
 
